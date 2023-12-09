916d7087c529426610f26f5107bdd66fa96359e5
adamwise <awise.developer@gmail.com>
2023-12-08 23:25:46 -0800

    trying again

--- src/main/java/objects/Dice.java
+++ src/main/java/objects/Dice.java
@@ -42,4 +42,4 @@ import java.util.Random;
             return "A " + sides + " sided die";
         }
     }
-}
+
--- src/test/java/com/example/circle_ci_tryagain/CircleCiTryagainApplicationTests.java
+++ src/test/java/com/example/circle_ci_tryagain/CircleCiTryagainApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.circle_ci_tryagain;
 
+import objects.Dice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class CircleCiTryagainApplicationTests {
@@ -10,4 +14,20 @@ class CircleCiTryagainApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void testRollReturnsValueWithinRange(){
+        Dice dice = new Dice(6,"red");
+        int roll = dice.roll();
+        assertTrue(roll >= 1 && roll <= 6);
+    }
+
+    @Test
+    void testRollManyReturnsCorrectArrayLength(){
+        Dice dice = new Dice(6,"red");
+        int[] rolls = dice.rollMany(5);
+        assertEquals(5,rolls.length);
+        for (int roll : rolls) {
+            assertTrue(roll >= 1 && roll <= 6);
+        }
+    }
 }
