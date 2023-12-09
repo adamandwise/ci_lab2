7371aea907e791d45c3dca6f33aaf8a61c686630
adamwise <awise.developer@gmail.com>
2023-12-08 23:09:50 -0800

    trying again

---
+++ src/main/java/com/example/circle_ci_tryagain/CircleCiTryagainApplication.java
@@ -0,0 +1,13 @@
+package com.example.circle_ci_tryagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiTryagainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleCiTryagainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/circle_ci_tryagain/CircleCiTryagainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circle_ci_tryagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiTryagainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
